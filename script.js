72d6c01a20f66b56d52781f96e26dd1c1d56a35f
Addr678 <128295657+Addr678@users.noreply.github.com>
2023-06-05 12:59:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,71 +1,108 @@
 //your JS code here. If required.
-var questions = [
-            {
-                question: "Which language runs in a web browser?",
-                a: "Java",
-                b: "C",
-                c: "Python",
-                d: "JavaScript",
-                correct: "d",
-            },
-            {
-                question: "What does CSS stand for?",
-                a: "Central Style Sheets",
-                b: "Cascading Style Sheets",
-                c: "Cascading Simple Sheets",
-                d: "Cars SUVs Sailboats",
-                correct: "b",
-            },
-            {
-                question: "What does HTML stand for?",
-                a: "Hypertext Markup Language",
-                b: "Hypertext Markdown Language",
-                c: "Hyperloop Machine Language",
-                d: "Helicopters Terminals Motorboats Lamborginis",
-                correct: "a",
-            },
-            {
-                question: "What year was JavaScript launched?",
-                a: "1996",
-                b: "1995",
-                c: "1994",
-                d: "none of the above",
-                correct: "b",
-            }
-        ];
-
-        var currentQuestion = 0;
-        var score = 0;
-
-        function loadQuestion() {
-            var question = questions[currentQuestion];
-            document.getElementById("question").textContent = question.question;
-            document.getElementById("a_text").textContent = question.a;
-            document.getElementById("b_text").textContent = question.b;
-            document.getElementById("c_text").textContent = question.c;
-            document.getElementById("d_text").textContent = question.d;
+      const quizData = [
+    {
+        question: "Which language runs in a web browser?",
+        a: "Java",
+        b: "C",
+        c: "Python",
+        d: "JavaScript",
+        correct: "d",
+    },
+    {
+        question: "What does CSS stand for?",
+        a: "Central Style Sheets",
+        b: "Cascading Style Sheets",
+        c: "Cascading Simple Sheets",
+        d: "Cars SUVs Sailboats",
+        correct: "b",
+    },
+    {
+        question: "What does HTML stand for?",
+        a: "Hypertext Markup Language",
+        b: "Hypertext Markdown Language",
+        c: "Hyperloop Machine Language",
+        d: "Helicopters Terminals Motorboats Lamborginis",
+        correct: "a",
+    },
+    {
+        question: "What year was JavaScript launched?",
+        a: "1996",
+        b: "1995",
+        c: "1994",
+        d: "none of the above",
+        correct: "b",
+    },
+];
+ 
+// const questions = [{},{},{},{}]
+ 
+const quiz = document.getElementById("quiz")
+const answerEls = document.querySelectorAll(".answer")
+const questionEl = document.getElementById("question")
+ 
+const submitBtn = document.getElementById("submit")
+ 
+const a_text = document.getElementById("a_text")
+const b_text = document.getElementById("b_text")
+const c_text = document.getElementById("c_text")
+const d_text = document.getElementById("d_text")
+ 
+let score = 0
+ 
+let currentQuestion = 0
+ 
+function loadQuestion(){
+ 
+    deselectAnswers()
+    const currentQuizData = quizData[currentQuestion]
+ 
+    questionEl.innerText = currentQuizData.question
+ 
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+}
+ 
+function deselectAnswers(){
+    answerEls.forEach(answerEl=> answerEl.checked=false)
+}
+ 
+function getSelected(){
+    let answer;
+ 
+    answerEls.forEach(answerEl=>{
+        if(answerEl.checked){
+            answer = answerEl.id
         }
-
-        function submitAnswer() {
-            var selectedOption = document.querySelector("input[name='option']:checked");
-            if (selectedOption) {
-                var selectedAnswer = selectedOption.value;
-                if (selectedAnswer === questions[currentQuestion].correct) {
-                    score++;
-                }
-                currentQuestion++;
-                if (currentQuestion < questions.length) {
-                    loadQuestion();
-                } else {
-                    showResult();
-                }
-            }
+    })
+    console.log(answer)
+    return answer
+}
+ 
+loadQuestion()
+ 
+submitBtn.addEventListener("click",()=>{
+ 
+    const answer = getSelected()
+ 
+    if(answer){
+        if(answer === quizData[currentQuestion].correct){
+            score++;
         }
-
-        function showResult() {
-            var result = document.getElementById("result");
-            result.textContent = "Your Score: " + score + " out of " + questions.length;
-            result.style.display = "block";
-            document.getElementById("quiz").style.display = "none";
+        currentQuestion++
+ 
+        if(currentQuestion<quizData.length){
+            loadQuestion()
+        }else{
+            quiz.innerHTML = `
+            <h2>
+                You answered ${score}/${quizData.length} questions correctly
+            </h2>
+            <button onclick="location.reload()">Reload</button>
+            `
         }
-   
+    }
+})  
+     
+        
